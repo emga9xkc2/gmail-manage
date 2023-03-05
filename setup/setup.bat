935f0b0b9d1029b04fada660a966c0b580eb596d
emga9xkc2 <android.24081995@gmail.com>
2023-03-05 12:10:03 +0700

    commit

--- setup/setup.bat
+++ setup/setup.bat
@@ -1,8 +1,8 @@
-
 @taskkill /IM "chromedriver.exe" /F
 @rmdir /s /q "%appdata%\myscript"
 @rmdir /s /q "%appdata%\nightowl\myscript"
 @rmdir /s /q "%temp%\gen_py"
+pip install -r requirements.txt
 powershell.exe -executionpolicy ByPass -File python.ps1
 @py ../main.pyc
 @timeout /t 100
