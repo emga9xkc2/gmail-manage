db081c0657f5c81eefb422fdee7aab3fb3373400
emga9xkc2 <android.24081995@gmail.com>
2023-03-09 13:04:50 +0700

    commit

--- script/_hotreload.py
+++ script/_hotreload.py
@@ -6,14 +6,35 @@ import sys
 sys.path.append("..")
 
 from hstr import *
+from hrand import *
 from hplaywrightExtended import ExtendedLocator, ExtendedPage
 
-def function1(self):
+def f1(self):
+
     p: ExtendedPage = self.p
-    phoneNumberId = p.locatorId("phoneNumberId")
-    if phoneNumberId.displayed():
-        print("phone")
-    time.sleep(2)
+    for i in range(2):
+        time.sleep(1)
+        return
+        url = p.url
+        if "https://accounts.google.com/signin/v2/disabled/appeal/additionalinformation" in url:
+            textarea = p.locatorSelector("textarea")
+            if textarea.is_visible():
+                content = hrand.randomItemInList(self.restoredisablecontent)
+                textarea.type(content, press="Enter")
+                textarea.waitHidden(10)
+        elif "https://accounts.google.com/signin/v2/disabled/appeal/confirmation" in url:
+            return
+        elif "https://accounts.google.com/signin/v2/disabled/appeal/contactaddress" in url:
+            input = p.locatorSelector('input[type="email"]')
+            if input.is_visible():
+                newmail = self.user.split("@")[0] + "@hotmail.com"
+                input.type(newmail, press="Enter")
+                input.waitHidden(10)
+        else:
+            batdaukhieunai = p.locatorSelector(".VfPpkd-vQzf8d").first
+            if batdaukhieunai.is_visible():
+                batdaukhieunai.click()
+        time.sleep(2)
 
 
 
