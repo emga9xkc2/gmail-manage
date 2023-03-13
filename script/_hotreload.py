3b44802207f056e53a4b0693d17e66eb928ee0b7
emga9xkc2 <android.24081995@gmail.com>
2023-03-13 10:30:33 +0700

    commit

--- script/_hotreload.py
+++ script/_hotreload.py
@@ -10,31 +10,21 @@ from hrand import *
 from hplaywrightExtended import ExtendedLocator, ExtendedPage
 
 def f1(self):
-
     p: ExtendedPage = self.p
-    for i in range(2):
+    url = p.url
+    if not "https://policies.google.com/terms" in url:
+        p.goto("https://policies.google.com/terms?hl=en")
+    timestart = time.time()
+    while time.time() - timestart < 2:
+        try:
+            source = p.content()
+            country = hstr.regex(source, "Country version:</a>(.*?)</p>").strip()
+            if country:
+                return  country
+        except Exception as e:
+            print(e)
         time.sleep(1)
-        return
-        url = p.url
-        if "https://accounts.google.com/signin/v2/disabled/appeal/additionalinformation" in url:
-            textarea = p.locatorSelector("textarea")
-            if textarea.is_visible():
-                content = hrand.randomItemInList(self.restoredisablecontent)
-                textarea.type(content, press="Enter")
-                textarea.waitHidden(10)
-        elif "https://accounts.google.com/signin/v2/disabled/appeal/confirmation" in url:
-            return
-        elif "https://accounts.google.com/signin/v2/disabled/appeal/contactaddress" in url:
-            input = p.locatorSelector('input[type="email"]')
-            if input.is_visible():
-                newmail = self.user.split("@")[0] + "@hotmail.com"
-                input.type(newmail, press="Enter")
-                input.waitHidden(10)
-        else:
-            batdaukhieunai = p.locatorSelector(".VfPpkd-vQzf8d").first
-            if batdaukhieunai.is_visible():
-                batdaukhieunai.click()
-        time.sleep(2)
+    return "Timeout"
 
 
 
