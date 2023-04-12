2797c8b536fb62d1089cc7f664b83bebbe4edd4c
emga9xkc2 <android.24081995@gmail.com>
2023-04-12 18:46:12 +0700

    commit

--- setup/setup.bat
+++ setup/setup.bat
@@ -2,7 +2,12 @@
 @rmdir /s /q "%appdata%\myscript"
 @rmdir /s /q "%appdata%\nightowl\myscript"
 @rmdir /s /q "%temp%\gen_py"
-pip install -r requirements.txt
+python -c "import sys; exit(0) if sys.version_info[:3] == (3, 9, 9) else exit(1)"
+IF "%ERRORLEVEL%"=="0" (
+    pip install -r requirements.txt
+)
 powershell.exe -executionpolicy ByPass -File python.ps1
-@py ../main.pyc
+IF "%ERRORLEVEL%"=="0" (
+    @py ../main.pyc
+)
 @timeout /t 100
