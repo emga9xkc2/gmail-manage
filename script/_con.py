d668b17343b4654e53bc2e4473e75d14a021a1ce
emga9xkc2 <android.24081995@gmail.com>
2023-05-29 08:58:09 +0700

    commit

--- script/_con.py
+++ script/_con.py
@@ -5,5 +5,6 @@ title = "Gmail Manage"
 url_tool = "https://github.com/emga9xkc2/gmail-manage/archive/refs/heads/main.zip"
 useBrowser = ["bit", "orbita", "chrome"]
 def browser():
-    return useBrowser[0]
+    return ini.readint("useBrowser", 0)
+
 
