a705f5459abe06f56f322785695ab045ac38ae4b
emga9xkc2 <android.24081995@gmail.com>
2023-05-29 08:49:21 +0700

    commit

--- script/_con.py
+++ script/_con.py
@@ -3,3 +3,7 @@ from hini import *
 ini = hini()
 title = "Gmail Manage"
 url_tool = "https://github.com/emga9xkc2/gmail-manage/archive/refs/heads/main.zip"
+useBrowser = ["bit", "orbita", "chrome"]
+def browser():
+    return useBrowser[0]
+
