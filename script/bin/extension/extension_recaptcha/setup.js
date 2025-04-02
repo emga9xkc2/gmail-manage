d5a17c5627623e1d729ff7f3103171f1c0687b28
emga9xkc2 <android.24081995@gmail.com>
2025-04-02 22:29:29 +0700

    commit

--- script/bin/extension/extension_recaptcha/setup.js
+++
@@ -1,1 +0,0 @@
-!function(){function e(){browser.permissions.request({origins:["<all_urls>","*://*.google.com/recaptcha/*","*://*.recaptcha.net/recaptcha/*"]})}document.getElementById("setup-button").addEventListener("click",e)}();
